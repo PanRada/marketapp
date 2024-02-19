294c2c5ded0f20f17b9ce910906dcc40976f9590
Sebastian <panradaproduction@gmail.com>
2024-02-19 11:31:30 +0100

    Add mock images for testing

---
+++ src/mocks/images.ts
@@ -0,0 +1,17 @@
+// Example images
+const images = [
+  {
+    uri: 'https://plus.unsplash.com/premium_photo-1664197368315-6e8f91db9215?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    id: '1',
+  },
+  {
+    uri: 'https://images.unsplash.com/photo-1639117474927-58d2d5a45b45?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    id: '2',
+  },
+  {
+    uri: 'https://images.unsplash.com/photo-1639117474927-58d2d5a45b45?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    id: '3',
+  },
+];
+
+export default images;
